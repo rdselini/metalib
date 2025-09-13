81cb6820a2a8bb67cc3f36d6847d0434f57af1db
rdselini <rdselini@github.com>
2025-09-13 12:24:17 +0000

    Initial commit

---
+++ git.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+class GitMetadata {
+public:
+	// Is the metadata populated? We may not have metadata if
+	// there wasn't a .git directory (e.g. downloaded source
+	// code without revision history).
+	static bool Populated();
+
+	// Were there any uncommitted changes that won't be reflected
+	// in the CommitID?
+	static bool AnyUncommittedChanges();
+
+	// The commit author's name.
+	static std::string AuthorName();
+	// The commit author's email.
+	static std::string AuthorEmail();
+	// The commit SHA1.
+	static std::string CommitSHA1();
+	// The ISO8601 commit date.
+	static std::string CommitDate();
+	// The Unix commit date
+	static std::int64_t CommitDateSecsSinceEpoch();
+	// The ISO8601 author date
+	static std::string AuthorDate();
+	// The Unix author date
+	static std::int64_t AuthorDateSecsSinceEpoch();
+	// The commit subject.
+	static std::string CommitSubject();
+	// The commit body.
+	static std::string CommitBody();
+	// The commit describe.
+	static std::string Describe();
+	// The current branch
+	static std::string Branch();
+};
